9e8b21f64575cacc69b5d2b4f939f761bcc35720
Tee Blagdon <tjlay23@gmail.com>
2016-08-05 12:09:26 -0700

    Feat: Handle getAll request for Tests

---
+++ server/api/test/test.router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import testController from './test.controller';
+
+const testRouter = express.Router();
+
+testRouter.get('/', testController.getTests);
+
+export default testRouter;
