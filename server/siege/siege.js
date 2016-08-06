b302597783dec3eae8d9a6e605a168db3e26426c
Tee Blagdon <tjlay23@gmail.com>
2016-08-06 16:30:44 -0700

    Update: Update runSiege url to loadBalancerURL

--- server/siege/siege.js
+++ server/siege/siege.js
@@ -3,12 +3,14 @@ import cmd from 'node-cmd';
 /**
  * function runSiege - runs siege test and logs response time for each request in siegelog.txt
  *
- * @param  {[STRING]} URL     [url to run siege test]
  * @param  {[INTEGER]} volume [number of concurrent users]
  */
-export default runSiege = (url, volume) {
 
-  cmd.get(`siege ${url} -c${volume} > siegelog.txt`, data => console.log(`Siege data ${data}`));
+const loadBalancerURL = 'http://52.8.16.173:9000/iptables';
+
+export default runSiege = (volume) {
+
+  cmd.get(`siege ${loadBalancerURL} -c${volume} > siegelog.txt`, data => console.log(`Siege data ${data}`));
 
 };
 
