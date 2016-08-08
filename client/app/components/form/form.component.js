7a36299bf50596a8d5743636a77eacef83080da7
Tee Blagdon <tjlay23@gmail.com>
2016-08-08 11:44:50 -0700

    Feat: Connect client form to API server

--- client/app/components/form/form.component.js
+++ client/app/components/form/form.component.js
@@ -10,9 +10,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var forms_1 = require('@angular/forms');
-var forms_2 = require('@angular/forms');
 var ipPort_1 = require('./ipPort');
-var ipPort_2 = require('./ipPort');
 var form_service_1 = require('./formServices/form.service');
 var http_1 = require('@angular/http');
 var FormComponent = (function () {
@@ -21,16 +19,11 @@ var FormComponent = (function () {
         this.types = ['web server', 'image processor', 'other'];
         this.application_type = 'other';
         this.application_type2 = 'other';
-        this.numReqModel = new ipPort_2.numReq(0);
         this.model = new ipPort_1.ipPort('123.456.789', '8080', 'web processor');
         this.model2 = new ipPort_1.ipPort('123.456.789', '8080', 'web processor');
-        this.model3 = new ipPort_1.ipPort('123.456.789', '8080', 'web processor');
-        this.submitted = false;
+        this.numReqModel = new ipPort_1.numReq(0);
     } // form builder simplify form initialization
     FormComponent.prototype.onSubmit = function () {
-        console.log(this.model);
-        console.log('numReq', this.numReqModel.numReq);
-        console.log([this.model, this.model2], this.numReqModel.numReq);
         this._FormService.sendTest({ 'servers': [this.model, this.model2], 'volume': this.numReqModel.numReq });
     };
     FormComponent.prototype.onChange = function (value) {
@@ -45,7 +38,7 @@ var FormComponent = (function () {
         core_1.Component({
             selector: 'my-form',
             templateUrl: "./client/app/components/form/form.component.html",
-            directives: [forms_2.FORM_DIRECTIVES, forms_1.REACTIVE_FORM_DIRECTIVES],
+            directives: [forms_1.FORM_DIRECTIVES, forms_1.REACTIVE_FORM_DIRECTIVES],
             providers: [form_service_1.FormService, http_1.HTTP_PROVIDERS]
         }), 
         __metadata('design:paramtypes', [form_service_1.FormService])
@@ -53,52 +46,4 @@ var FormComponent = (function () {
     return FormComponent;
 }());
 exports.FormComponent = FormComponent;
-// import { Component, OnInit } from '@angular/core';
-// import { Validators } from '@angular/common';
-// import { NgForm }    from '@angular/common';
-// import {  
-//   REACTIVE_FORM_DIRECTIVES, 
-//   FormGroup, 
-//   FormControl, 
-//   FormBuilder
-// } from '@angular/forms';
-// import {FORM_DIRECTIVES} from '@angular/forms';
-// import { ipPort } from './ipPort';
-// import { FormService } from './formServices/form.service';
-// import { HTTP_PROVIDERS } from '@angular/http';
-// @Component({
-//   selector: 'my-form',
-//   templateUrl: "./client/app/components/form/form.component.html",
-//   directives: [FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES],
-//   providers: [FormService]
-// })
-// export class FormComponent { 
-//   constructor(private _FormService: FormService) { 
-//   } // form builder simplify form initialization
-//   types = ['web server', 'image processor', 'other'];
-//   type = 'other';
-//   type2 = 'other';
-//   type3 = 'other';
-//   numReq = 0;
-//   model = new ipPort('123.456.789', '8080', 'web processor');
-//   model2 = new ipPort('123.456.789', '8080', 'web processor');
-//   model3 = new ipPort('123.456.789', '8080', 'web processor');
-//   submitted = false;
-//   onSubmit() { 
-//     console.log(this.model);
-//     this._FormService.sendTest([this.model]);
-//   }
-//   onChange(value){
-//     this.type = value;
-//     this.model.type =value;
-//   }
-//   onChange2(value){
-//     this.type2 = value;
-//     this.model2.type =value;
-//   }
-//   onChange3(value){
-//     this.type3 = value;
-//     this.model3.type =value;
-//   }
-// }
 //# sourceMappingURL=form.component.js.map
