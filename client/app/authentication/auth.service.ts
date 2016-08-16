deda65f22bb70244e14e069d4c007dfa92f30d21
James Ramadan <jamesramadan@gmail.com>
2016-08-16 10:45:07 -0700

    Commiting to switch branches. Midway through LinkingAccounts.

--- client/app/authentication/auth.service.ts
+++ client/app/authentication/auth.service.ts
@@ -1,6 +1,7 @@
 /* ===== app/auth.service.ts ===== */
 import { Injectable }      from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt/angular2-jwt';
+import { AuthHttp } from 'angular2-jwt/angular2-jwt';
 
 // Avoid name not found warnings
 declare var Auth0Lock: any;
@@ -20,6 +21,14 @@ export class Auth {
         }]
   });
 
+  lockLink = new Auth0Lock('lSGQqNGvDdE2GQdwFCQ9det1PCZUEU5q', 'jamesramadan.auth0.com', {
+      auth: {params: {state: "linking"}},
+      allowedConnections: ['Username-Password-Authentication', 'facebook', 'google-oauth2'],
+      languageDictionary: { // allows to override dictionary entries
+        title: "Link with:"
+      }
+    });
+
   //Store profile object in auth class
     userProfile: Object;
 
@@ -30,19 +39,38 @@ export class Auth {
 
     // Add callback for lock `authenticated` event
     this.lock.on("authenticated", (authResult) => {
-      localStorage.setItem('id_token', authResult.idToken);
-      // Fetch profile information
-      this.lock.getProfile(authResult.idToken, (error, profile) => {
-        if (error) {
-          // Handle error
-          alert(error);
-          return;
-        }
 
-        profile.user_metadata = profile.user_metadata || {};
-        localStorage.setItem('profile', JSON.stringify(profile));
-        this.userProfile = profile;
+
+      if(authResult.state != "linking"){
+        localStorage.setItem('id_token', authResult.idToken);
+        this.fetchProfile(authResult.idToken);
+      }
+
+      // Add callback for lockLink `authenticated` event
+      this.lockLink.on("authenticated", (authResult) => {
+        // Every lock instance listens to the same event, so you have to check if
+        // it's the linking login here.
+        if(authResult.state == "linking"){
+          // If it's the linking login, then create the link through the API.
+          this.doLinkAccounts(authResult.idToken);
+        }
       });
+
+
+      // localStorage.setItem('id_token', authResult.idToken);
+      // // Fetch profile information
+      // this.lock.getProfile(authResult.idToken, (error, profile) => {
+      //   if (error) {
+      //     // Handle error
+      //     alert(error);
+      //     return;
+      //   }
+
+      //   profile.user_metadata = profile.user_metadata || {};
+      //   localStorage.setItem('profile', JSON.stringify(profile));
+      //   this.userProfile = profile;
+      // });
+
     });
   }
 
