2d6e2081d40f436964b9f9261210f87413652320
andrew brown <aebrownz11@gmail.com>
2016-08-18 12:07:22 -0700

    Added shell scripts for AWS CLI

--- server/config/spinupec2.sh
+++ server/config/spinupec2.sh
@@ -1,3 +1,2 @@
 #need to provide credentials to login and fix path
-
 aws ec2 run-instances --image-id ami-b57536d5 --count 1 --instance-type t2.micro --key-name awskey1 --security-groups launch-wizard-3 --user-data file:///Users/andrewbrown/startlb.sh
