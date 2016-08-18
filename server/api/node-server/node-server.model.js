4eb695ab5f4eae6385812a189b1cfb5f20858a28
Tee Blagdon <tjlay23@gmail.com>
2016-08-17 20:01:01 -0700

    Feat: Set unqiue constraint for ip field in NodeServer model

--- server/api/node-server/node-server.model.js
+++ server/api/node-server/node-server.model.js
@@ -1,7 +1,11 @@
 export default function NodeServer(db, DataTypes) {
 
   return db.define('NodeServer', {
-    ip: DataTypes.STRING,
+    ip: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
     port: DataTypes.STRING,
     application_type: DataTypes.STRING
   });
