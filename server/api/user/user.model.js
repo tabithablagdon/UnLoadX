43b2df7cbd49bad849f188552011e81adc02ea69
Tee Blagdon <tjlay23@gmail.com>
2016-08-17 20:04:03 -0700

    Feat: Remove username and password fields and add authUserId field

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -1,8 +1,10 @@
 export default function User(sequelize, DataTypes) {
   return sequelize.define('User', {
     name: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    authUserId: {
+      type: DataTypes.STRING,
+      unique: true
+    },
     email: DataTypes.STRING
   });
 }
