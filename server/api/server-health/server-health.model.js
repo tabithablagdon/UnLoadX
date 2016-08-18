a5f412d88a51847bb73b1d6f8199d0bf5dd9ee31
Tee Blagdon <tjlay23@gmail.com>
2016-08-18 16:09:24 -0700

    Feat: Add 'available' field to ServerHealth model and controller

--- server/api/server-health/server-health.model.js
+++ server/api/server-health/server-health.model.js
@@ -2,7 +2,8 @@ export default function ServerHealth(db, DataTypes) {
 
   return db.define('ServerHealth', {
     memory: DataTypes.FLOAT,
-    CPU: DataTypes.FLOAT
+    CPU: DataTypes.FLOAT,
+    available: DataTypes.BOOLEAN
   });
 
 };
