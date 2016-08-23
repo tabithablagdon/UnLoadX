4c61061cefcc7b53275ece125bc9ede4eeda1cc5
andrew brown <aebrownz11@gmail.com>
2016-08-22 20:19:06 -0700

    Miscellaneous fixes
    Added conditional to bash script to check env variable before spinning up ec2 instance
    Modified docker yml to run typescript compiler before starting node
    Removed some commented out code in server
    Deleted two old shell scripts that were in the wrong directory

--- server/server.js
+++ server/server.js
@@ -5,29 +5,6 @@ import db from './db/db';
 import nodeController from './api/node-server/node-server.controller';
 import requestController from './api/request/request.controller';
 
-// //
-// import LB_Ready from './config/LB_Ready.js';
-// import async from 'async';
-
-// //
-// import LB_Ready from './config/LB_Ready.js';
-// import async from 'async';
-
-// const filename = __dirname + '/config/dummyArraydata.txt';
-// const TBDRestEndPoint = '/';
-// let LB_IP = '54.153.124.136';
-
-// setInterval(() => {
-//     LB_Ready.get200fromLB(LB_IP, TBDRestEndPoint)
-//     .then(results => {
-//       IP = results;
-//       console.log(IP);
-//     });
-// }, 10000);
-
-// //
-
-
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -42,9 +19,9 @@ io.on('connection', (socket) => {
 
   socket.on('receive-post', (requests) => {
     console.log('Received POST from client socket!', requests);
-    nodeController.createServerNodeSocket(requests) // returns dataFromLB = {'Volume': 10, 'TestId': 2}
-      .then(dataFromLB => nodeController.startSiege(dataFromLB)) // returns requestBody = {requests: parsedDataArray, testId: testId}
-      .then(requestBody => requestController.createRequest(requestBody)) // returns statsData  = {testId: 1, totalReqs: 400, latency: {latencySet, avg, max, min, stdDev}, status }-=
+    nodeController.createServerNodeSocket(requests)
+      .then(dataFromLB => nodeController.startSiege(dataFromLB)) 
+      .then(requestBody => requestController.createRequest(requestBody))
       .then(statsData => {
         console.log('[STEP 9]: Received statsData back from requestController - sending back up to client - statsData is: ', statsData);
         socket.emit('receive-requests', statsData);
