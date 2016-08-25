9b66eafd7386b21907c9da499385ed0ef5bed0e3
andrew brown <aebrownz11@gmail.com>
2016-08-24 17:17:12 -0700

    Modified LB polling function to poll for a set period
    Recursively return promises for 10 minutes, or until LB responds with 200
    Added this function to the promise chain in the user signup flow

--- server/config/LB_Ready.js
+++ server/config/LB_Ready.js
@@ -1,52 +1,42 @@
 'use strict'
-// import fs from 'fs';
-// import Promise from 'bluebird';
-// import request from 'request';
-// import async from 'async';
-const fs = require('fs');
-const Promise = require('bluebird');
 const request = require('request');
-const async = require('async');
 
 const LB_Ready = {};
 
-LB_Ready.parseLBPublicIPAddress = (filename) => {
+LB_Ready.get200fromLB = (parsedPublicIP) => {
 
-  console.log(`parsing LB_IP`);
-  return new Promise((resolve, reject) => {
-    fs.readFile(filename, 'utf8', (err, data) => {
-      if (err) {
-        console.log(`Error reading file ${err.message}`);
-        reject(err);
-      } else {
-        console.log('data',data);
-        let dataArray = data.toString().replace(/ /g,'').split('\n');
-        let parsedPublicIP = dataArray[1].slice(2).slice(0,-1);
-        console.log('inArray1', parsedPublicIP);
-        resolve(parsedPublicIP);
-      }
-    });
-  });
-};
+  const options = {
+    timeout: 5000,
+    uri: `http://${parsedPublicIP}:9000/`,
+  }
 
-LB_Ready.get200fromLB = (parsedPublicIP, TBDRestEndPoint) => {
-  console.log(`getting 200 Status from LB`);
-  return new Promise((resolve, reject) => {
-    let path = `http://${parsedPublicIP}:9000${TBDRestEndPoint}/`
-    console.log('path: ', path)
-    exec(`curl ${path}`, (err, res, body) => {
-      if (err) {
-        console.error(`exec error: ${err}`);
-        console.log(`res: ${res}, body: ${body}`);
-        reject(err);
+  function checkEc2Avail() {
+    console.log('inec2avail')
+    return new Promise((resolve, reject) => {
+      let attempt = 0;
+      const httpRecursion = attempt => {
+        request(options, (err, resp, body) => {
+          if (err) {
+            console.log('err')
+            if (attempt < 60) {
+              console.log(`not yet, attempt ${attempt}`)
+              httpRecursion(++attempt);
+            } else {
+              console.log('60 attempts, done')
+              reject(err);
+            }
+          } else {
+            console.log('success?')
+            resolve();
+          }
+        });
       }
-      if (body) {
-        console.log('body', body);
-        console.log('path', path)
-        resolve(path);
-      }
+      httpRecursion(attempt);
     });
-  });
-};
+  }
+  return checkEc2Avail()
+    .then(() => console.log('it worked!'))
+    .catch(err => console.log(`${err}`))
+}
+
 module.exports = LB_Ready;
-// LB_Ready.get200fromLB('127.0.0.1', '/')
